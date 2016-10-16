5b3aa95b58d494e7fb2f4f7f53e9cf528c81414a
DoDemDishes <mr.raczynski1337@gmail.com>
2016-10-16 14:11:16 +0200

    Warunek na nie nakladanie sie czastek

--- zad2.py
+++ zad2.py
@@ -10,19 +10,31 @@ class czastka:
 
 gaz = [] # zbiornik na czastki
 liczba_czastek = 100
-c = plt.cm.summer(0.5)
-fig, ax = plt.subplots( figsize=(8,8) )
+promien = 0.02
+fig, ax = plt.subplots(figsize=(8,8))
 
-for i in xrange(liczba_czastek):
-    polozenie = np.array([np.random.uniform(0.,1.), np.random.uniform(0.,1.)])
-    predkosc = np.array([np.random.uniform(0.,1.), np.random.uniform(0.,1.)])
-    andrzej = czastka(0.02,polozenie,predkosc)
-    gaz.append(andrzej)
+def losowanie (liczba_czastek, promien, tabela):
+    for i in xrange(liczba_czastek):
+        polozenie = np.array([np.random.uniform(0.,1.), np.random.uniform(0.,1.)])
+        predkosc = np.array([np.random.uniform(0.,1.), np.random.uniform(0.,1.)])
+        nakladanie = False
+        for i in gaz:
+            if ((i.r[0] - polozenie[0])**2 + (i.r[1] - polozenie[1])**2) < (2*promien)**2:
+                nakladanie = True
+                print 'najezdca'
+        if nakladanie == False:
+            andrzej = czastka(promien, polozenie, predkosc)
+            gaz.append(andrzej)
+
+while (len(gaz) < liczba_czastek):
+    losowanie(liczba_czastek, promien, gaz)
 
 for i in xrange(liczba_czastek):
     c = plt.cm.summer(np.linalg.norm(gaz[i].v))
-    kolko = plt.Circle((gaz[i].r), 0.02, color = c)
+    kolko = plt.Circle((gaz[i].r), promien, color = c)
     ax.add_artist(kolko)
-
 fig.savefig('kolka.png')
+print len(gaz)
+for i in gaz:
+    print i.r
 plt.show()
