c139f10a50af77cc4db0f9e03c05e8e25729e711
DoDemDishes <mr.raczynski1337@gmail.com>
2016-11-27 18:52:22 +0100

    fraktale

---
+++ _fraktalbartka.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import array, zeros
+from numpy.random import randint, random
+
+m = [0]*2
+#p = 0
+# trojkat Sierpinskiego
+m[0] = array([[0.5,0.,0.,0.5,0.,0.],[0.5,0.,0.,0.5,0.5,0.],[0.5,0.,0.,0.5,0.25,np.sqrt(3./4)]])
+
+# paprotka
+m[1] = array([[0.85,0.04,-0.04,0.85,0.,1.6],[0.2,-0.26,0.23,0.22,0.,1.6],[-0.15,0.28,0.26,0.24,0.,0.44],[0.,0.,0.,0.16,0.,0.]])
+
+
+
+def create_table(nr,liczba_iteracji):
+	p = None
+	tmp = None
+	for i in xrange(100000):
+		if nr == 0:
+			p = randint(0,3)
+		if nr == 1:
+			tmp = random()
+			if tmp < 0.73: 	p = 0
+			elif tmp < 0.86: p = 1
+			elif tmp < 0.97: p = 2
+			else: p = 3
+		tab[p] += 1
+		xpoints.append(m[nr][p,0]*xpoints[-1] + m[nr][p,1]*ypoints[-1] + m[nr][p,4])
+		ypoints.append(m[nr][p,2]*xpoints[-2] + m[nr][p,3]*ypoints[-1] + m[nr][p,5])
+
+def siatka():
+	stosunek = []
+	tab_N = []
+	for N in xrange(1,liczba_podzialow+1):
+		pole = zeros([2**N,2**N])
+		ax = np.floor( (xpoints - x_min) / (x_max - x_min + 0.0001) * 2**N)
+		ay = np.floor( (ypoints - y_min) / (y_max - y_min + 0.0001) * 2**N)
+		for k in xrange(len(xpoints)):
+			pole[ax[k], ay[k]] = 1
+		stosunek.append(np.sum(pole))
+		tab_N.append(2**N)
+	return [tab_N,stosunek]
+
+tab = [0]*4
+
+nr = 1
+liczba_iteracji = 100000
+liczba_podzialow = 8
+
+xpoints = [0.]
+ypoints = [0.]
+
+create_table(nr, liczba_iteracji)
+#print np.array(tab)/float(liczba_iteracji)
+
+
+ax = plt.gca()
+ax.set_axis_bgcolor('black')
+plt.figure(1)
+plt.scatter(xpoints, ypoints, s=1, marker=".", lw=0, c='y')
+#plt.show()
+#plt.savefig('paprotka.png')
+
+x_min = min(xpoints)
+x_max = max(xpoints)
+y_min = min(ypoints)
+y_max = max(ypoints)
+
+
+xpoints = array(xpoints)
+ypoints = array(ypoints)
+
+siatka = siatka()
+
+print siatka[0]
+print siatka[1]
+
+iksy = np.log(np.array(siatka[0]))
+igreki = np.log(np.array(siatka[1]))
+
+print iksy
+print igreki
+
+plt.figure(2)
+#plt.scatter(siatka[0], siatka[1], s=1, marker=".", lw=0, c='y')
+plt.scatter(iksy,igreki,marker="o")
+#plt.show()
+plt.savefig('paprotka_fit.png')
+
+coeff = np.polyfit(iksy,igreki,deg=1)
+print coeff
+
--- fraktale.py
+++ fraktale.py
@@ -1,18 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import matplotlib.path as mplPath
 
 t = 0
 r = np.array([0,0])
 points = list()
+dimensions = list()
+
+def iteration(r,m):
+    r = [m[0]*r[0] + m[1]*r[1] + m[4], m[2]*r[0] + m[3]*r[1] + m[5]]
+    return r
+
+def generate_net(points, k):
+    net = list()
+    x_coordinates = np.array(np.linspace(min(zip(*points)[0]) , max(zip(*points)[0]), k))
+    y_coordinates = np.array(np.linspace(min(zip(*points)[1]) , max(zip(*points)[1]), k))
+
+    net.append(x_coordinates)
+    net.append(y_coordinates)
+    # print net
+    return net
 
 choice1 = input("Co rysujemy? \n 1 - Trojkaty Sierpinskiego \n 2 - Paprotki Barnsley'a \n")
 choice2 = int(input("Liczba iteracji? \n"))
 
 if choice1 == 1:
-    def iteration(r,m):
-        r = [m[0]*r[0] + m[1]*r[1] + m[4], m[2]*r[0] + m[3]*r[1] + m[5]]
-        return r
     while t < choice2:
         ##generowanie liczby od 1 do 3
         random = np.random.randint(1,4)
@@ -30,9 +43,6 @@ if choice1 == 1:
             points.append(r)
         t += 1
 elif choice1 == 2:
-    def iteration(r,m):
-        r = [m[0]*r[0] + m[1]*r[1] + m[4], m[2]*r[0] + m[3]*r[1] + m[5]]
-        return r
     while t < choice2:
         ##generowanie liczby od 1 do 100
         random = np.random.randint(1,101)
@@ -56,10 +66,35 @@ elif choice1 == 2:
 else:
     print "Zly wybor, sprobuj jeszcze raz"
 
+## Liczenie rozmiaru pudelkowego
+for k in xrange(1,7):
+    nk = 0
+    for i in xrange(0,int(math.pow(2,k))):
+        for j in xrange(0,int(math.pow(2,k))):
+            # print 'i: ', i, ' j: ', j
+            includes = False
+            net = generate_net(points, math.pow(2,k)+1)
+            # print net
+            poly = [net[0][i],net[0][i+1],net[1][j],net[1][j+1]]
+            # print poly
+            bbPath = mplPath.Path(np.array([[poly[0], poly[1]],
+                         [poly[1], poly[2]],
+                         [poly[2], poly[3]],
+                         [poly[3], poly[0]]]))
+            for z in xrange(choice2):
+                includes = bbPath.contains_point((points[z][0], points[z][1]))
+                if includes == True:
+                    nk += 1
+                    break
+    # dimensions.append([nk,k])
+    dimensions.append([math.log(nk), k*math.log(2)])
 
-print "max x: ", max(zip(*points)[0]) , "min x:", min(zip(*points)[0])
-print "max y: ", max(zip(*points)[1]) , "min y:", min(zip(*points)[1])
-
+np.savetxt("Paprotki_1m.csv", dimensions, delimiter=",")
+# print dimensions
 zip(*points)
+plt.figure(1)
+plt.subplot(211)
 plt.scatter(*zip(*points))
-plt.show()
+plt.subplot(212)
+plt.scatter(*zip(*dimensions))
+# plt.show()
