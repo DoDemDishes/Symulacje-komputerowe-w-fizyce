84cda581ce09d658e2f6ab4b916c3fef3c5d0272
DoDemDishes <mr.raczynski1337@gmail.com>
2016-12-05 17:59:45 +0100

    reakcje

--- reakcja.py
+++ reakcja.py
@@ -7,7 +7,7 @@ D_u = 2*10**(-5)
 D_v = 1*10**(-5)
 F = 0.025
 k = 0.055
-dx = 0.2
+dx = 0.02
 dt = 1
 en = 1
 
@@ -22,13 +22,14 @@ for i in range(N/4,3*N/4):
     v[i] = np.random.random()*0.2 + 0.2
 
 while 1:
-    u_new = (D_u * (((np.roll(u,1) - np.roll(u,-1)-2*u))/dx**2) - u*v**2 + F*(1-u))*dt
-    v_new = (D_v * (((np.roll(v,1) - np.roll(v,-1)-2*u))/dx**2) + u*v**2 - v*(F+k))*dt
+    u_new = (D_u * (((np.roll(u,1) + np.roll(u,-1)-2*u))/dx**2) - u*v**2 + F*(1-u))*dt
+    v_new = (D_v * (((np.roll(v,1) + np.roll(v,-1)-2*v))/dx**2) + u*v**2 - v*(F+k))*dt
     u += u_new
     v += v_new
     # print u_new
     # print v_new
-    plt.scatter(xs,v)
-    plt.savefig('reakcja/img' + str(en) + '.png')
-    plt.clf()
+    if en % 50 == 0:
+        plt.scatter(xs,v)
+        plt.savefig('reakcja/img' + str(en) + '.png')
+        plt.clf()
     en += 1
