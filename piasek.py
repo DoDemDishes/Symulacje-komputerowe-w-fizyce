24276de21928b093c688b747a8ac5ed3027a1234
DoDemDishes <mr.raczynski1337@gmail.com>
2016-12-16 07:20:52 +0100

    update

--- piasek.py
+++ piasek.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import csv
+from collections import Counter
 
 dim = (31,31)
 mapa = np.zeros(dim)
 t = 0
-tmax = 10000
+tmax = 15000
 temp = []
 liczba_czastek = []
 falaaaa = []
+stacjonarny = False
 
 while t < tmax:
         fala = 0
@@ -40,25 +43,42 @@ while t < tmax:
             temp = []
             fala += 1
             if test == False: break
-        falaaaa.append([t,fala])
+        if stacjonarny == True:
+            falaaaa.append(fala)
         x = np.random.randint(0,dim[0])
         y = np.random.randint(0,dim[1])
         mapa[x][y] += 1
         t += 1
         liczba_czastek.append([t,np.array([np.sum(mapa)])])
-
-print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in mapa]))
-# print np.sum(mapa)
-# fig=plt.figure()
-# ax=fig.add_subplot(111)
-# ax.set_title('Height of the Sandpile')
-# cax = ax.imshow(mapa, interpolation='nearest')
-# cax.set_clim(vmin=0, vmax=5)
-# cbar = fig.colorbar(cax, ticks=[0,1,2,3,4,5], orientation='vertical')
-# filename = str('%03d' % t) + '.png'
-# plt.savefig(filename, dpi=100)
-# plt.clf()
-# print liczba_czastek
-plt.scatter(*zip(*falaaaa))
-plt.show()
+        if t >= 3000:
+            stacjonarny = True
+wystepowanie = Counter(falaaaa)
+### printowanie macierzy
+# print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+#       for row in mapa]))
+### printowanie mapy
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.set_title('Height of the Sandpile')
+cax = ax.imshow(mapa, interpolation='nearest')
+cax.set_clim(vmin=0, vmax=5)
+cbar = fig.colorbar(cax, ticks=[0,1,2,3,4,5], orientation='vertical')
+filename = str('%03d' % t) + '.png'
+plt.savefig(filename, dpi=100)
+plt.clf()
+### printowanie liczby czastek od czasu
+plt.plot(*zip(*liczba_czastek))
+plt.xlabel('czas')
+plt.ylabel('liczba czastek')
+plt.savefig(str('%03d' % t)+'czastki_od_czasu.png')
+plt.clf()
+### printowanie histogramu
+plt.loglog(wystepowanie.keys(),wystepowanie.values())
+plt.xlabel('S')
+plt.ylabel('N(S)')
+plt.savefig(str('%03d' % t)+'loglog.png')
+### zapisywanie do csv
+with open('dict.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in wystepowanie.items():
+       writer.writerow([key, value])
